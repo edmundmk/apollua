5f29bf68e646d2c1a9552a3c3519677bee594a51
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2010-04-12 07:33:31 +0000

    string.format using CLR formatting plus munging the returned string.  Doeesn't handle octal or hex float formats.
    git-svn-id: http://svn.birotanker.com/Apollua@126 7c9f8f60-b31a-3540-8745-75118cae9e49

--- 2010/Lua5.1/Library/string.cs
+++ 2010/Lua5.1/Library/string.cs
@@ -5,6 +5,7 @@
 
 
 using System;
+using System.Globalization;
 using System.Text;
 using Lua.Interop;
 
@@ -29,9 +30,429 @@ public static partial class @string
 		string format = lua.Argument< string >( 0 );
 
 		StringBuilder s = new StringBuilder();
-		s.Append( format );
+
+		// Iterate through conversion specifications in format.
+		int argument = 1;
+		int start = 0;
+		while ( true )
+		{
+			// Find next format specification.
+			int p = format.IndexOf( '%', start );
+			if ( p == -1 )
+			{
+				s.Append( format.Substring( start ) );
+				break;
+			}
+
+			// Add chunk of format string.
+			s.Append( format.Substring( start, p - start ) );
+
+			// Parse conversion specification.
+			char c = format[ p++ ];
+
+			// Flags.
+			bool bLeftJustify	= false;
+			bool bSignPlus		= false;
+			bool bSignSpace		= false;
+			bool bAlternateForm	= false;
+			bool bPadWithZeroes	= false;
+
+			while ( true )
+			{
+				c = format[ p++ ];
+				switch ( c )
+				{
+					case '-':	bLeftJustify	= true;	continue;
+					case '+':	bSignPlus		= true;	continue;
+					case ' ':	bSignSpace		= true; continue;
+					case '#':	bAlternateForm	= true; continue;
+					case '0':	bPadWithZeroes	= true; continue;
+				}
+
+				break;
+			}
+
+			if ( bSignSpace && bSignPlus )
+				bSignSpace = false;
+			if ( bPadWithZeroes && bLeftJustify )
+				bPadWithZeroes = false;
+			
+
+			// Field width.
+			int fieldWidth = 0;
+
+			while ( c >= '0' && c <= '9' )
+			{
+				int digit = c - '0';
+				fieldWidth = fieldWidth * 10 + digit;
+
+				c = format[ p++ ];
+			}
+
+
+			// Precision.
+			int precision = -1;
+
+			if ( c == '.' )
+			{
+				precision = 0;
+				c = format[ p++ ];
+				
+				bool bNegativePrecision = false;
+				if ( c == '-' )
+				{
+					bNegativePrecision = true;
+					c = format[ p++ ];
+				}
+				
+				while ( c >= '0' && c <= '9' )
+				{
+					int digit = c - '0';
+					precision = precision * 10 + digit;
+
+					c = format[ p++ ];
+				}
+
+				if ( bNegativePrecision )
+				{
+					precision = -1;
+				}
+			}
+
+
+			// Conversion specifier cheracter.
+			switch ( c )
+			{
+
+			// Integer conversions.
+
+			case 'd': case 'i':
+			{
+				string value = IntegerFormat( lua.Argument< int >( argument++ ), "D", precision, ref bPadWithZeroes );
+				value = AddSign( value, bSignPlus, bSignSpace );
+				if ( bPadWithZeroes )
+					AppendPadded( s, value, fieldWidth );
+				else
+					AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+			case 'o':
+			{
+				throw new NotImplementedException();
+			}
+
+			case 'u':
+			{
+				string value = IntegerFormat( lua.Argument< uint >( argument++ ), "D", precision, ref bPadWithZeroes );
+				if ( bPadWithZeroes )
+					AppendPadded( s, value, fieldWidth );
+				else
+					AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+			case 'x': case 'X':
+			{
+				string f = c.ToString();
+				string value = IntegerFormat( lua.Argument< int >( argument++ ), f, precision, ref bPadWithZeroes );
+				if ( bAlternateForm )
+					value = "0" + f + value;
+				if ( bPadWithZeroes )
+					AppendPadded( s, value, fieldWidth );
+				else
+					AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+
+			// Floating-point conversions.
+
+			case 'f': case 'F':
+			{
+				double v = lua.Argument< double >( argument++ );
+				string value = ConvertInfinityOrNan( v, c );
+				if ( value == null )
+				{
+					if ( precision == -1 )
+						precision = 6;
+					value = v.ToString( "F" + precision.ToString() );
+					if ( bAlternateForm && precision == 0 )
+						value += ".";
+				}
+				value = AddSign( value, bSignPlus, bSignSpace );
+				if ( bPadWithZeroes )
+					AppendPadded( s, value, fieldWidth );
+				else
+					AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+		
+			case 'e': case 'E':
+			{
+				string f = c.ToString();
+				double v = lua.Argument< double >( argument++ );
+				string value = ConvertInfinityOrNan( v, c );
+				if ( value == null )
+				{
+					if ( precision == -1 )
+						precision = 6;
+					value = v.ToString( f + precision.ToString() );
+
+					// Remove extra exponent digit if it is 0.
+					value = value.Replace( f + NumberFormatInfo.CurrentInfo.PositiveSign + "0", f + NumberFormatInfo.CurrentInfo.PositiveSign );
+					value = value.Replace( f + NumberFormatInfo.CurrentInfo.NegativeSign + "0", f + NumberFormatInfo.CurrentInfo.NegativeSign );
+
+					// Add decimal point if forced.
+					if ( bAlternateForm )
+					{
+						int e = value.IndexOf( Char.IsUpper( c ) ? 'E' : 'e' );
+						if ( e == -1 )
+							e = value.Length;
+						if ( value.IndexOf( NumberFormatInfo.CurrentInfo.NumberDecimalSeparator ) == -1 )
+							value = value.Insert( e, NumberFormatInfo.CurrentInfo.NumberDecimalSeparator );
+					}
+				}
+				value = AddSign( value, bSignPlus, bSignSpace );
+				if ( bPadWithZeroes )
+					AppendPadded( s, value, fieldWidth );
+				else
+					AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+			
+			case 'g': case 'G':
+			{
+				string f = c.ToString();
+				double v = lua.Argument< double >( argument++ );
+				string value = ConvertInfinityOrNan( v, c );
+				if ( value == null )
+				{
+					if ( precision == -1 )
+						precision = 6;
+					if ( precision == 0 )
+						precision = 1;
+					value = v.ToString( f + precision.ToString() );
+
+					// Add in decimal point and trailing zeroes if alternate form.
+					if ( bAlternateForm )
+					{
+						int e = value.IndexOf( Char.IsUpper( c ) ? 'E' : 'e' );
+						if ( e == -1 )
+							e = value.Length;
+						if ( value.IndexOf( NumberFormatInfo.CurrentInfo.NumberDecimalSeparator ) == -1 )
+						{
+							value = value.Insert( e, NumberFormatInfo.CurrentInfo.NumberDecimalSeparator ); 
+							e += 1;
+						}
+						int length = precision + 1;
+						if ( value.StartsWith( NumberFormatInfo.CurrentInfo.NegativeSign ) )
+							length += NumberFormatInfo.CurrentInfo.NegativeSign.Length;
+						while ( e < length )
+						{
+							value = value.Insert( e, "0" );
+							e += 1;
+						}
+					}
+				}
+				value = AddSign( value, bSignPlus, bSignSpace );
+				if ( bPadWithZeroes )
+					AppendPadded( s, value, fieldWidth );
+				else
+					AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+			case 'a': case 'A':
+			{
+				throw new NotImplementedException();
+			}
+
+
+			// String conversions.
+
+			case 'c':
+			{
+				int value = lua.Argument< int >( argument++ );
+				AppendJustified( s, Char.ConvertFromUtf32( value ), fieldWidth, bLeftJustify );
+				break;
+			}
+
+			case 's':
+			{
+				string value = lua.Argument< string >( argument++ );
+				if ( precision != -1 && value.Length > precision )
+					value = value.Remove( precision );
+				AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+			case 'q':
+			{
+				string value = lua.Argument< string >( argument++ );
+				
+				StringBuilder q = new StringBuilder();
+				q.Append( '"' );
+				start = 0;
+				for ( int i = 0; i < value.Length; ++i )
+				{
+					string escape = null;
+					
+					switch ( value[ i ] )
+					{
+					case '"':	escape = "\\\"";	break;
+					case '\n':	escape = "\\n";		break;
+					case '\0':	escape = "\\0";		break;
+					case '\\':	escape = "\\\\";	break;
+					}
+
+					if ( escape != null )
+					{
+						q.Append( value, start, i - start );
+						q.Append( escape );
+						start = i + 1;
+					}
+				}
+				q.Append( value, start, value.Length - start ); 
+				q.Append( '"' );
+				
+				value = q.ToString();
+
+				if ( precision != -1 && value.Length > precision )
+					value = value.Remove( precision );
+				AppendJustified( s, value, fieldWidth, bLeftJustify );
+				break;
+			}
+
+			case '%':
+			{
+				s.Append( '%' );
+				break;
+			}
+
+
+			// Invalid conversion.
+
+			default:
+				throw new FormatException( "Invalid format conversion specifier." );
+			}
+
+			
+			start = p;
+		}
+		
 
 		lua.Return( s.ToString() );
+	}
+
+
+	static string IntegerFormat( long value, string format, int precision, ref bool bPadWithZeroes )
+	{
+		if ( precision >= 0 )
+		{
+			bPadWithZeroes = false;
+			format += precision.ToString();
+		}
+		return value.ToString( format );
+	}
+
+
+	static string AddSign( string value, bool bSignPlus, bool bSignSpace )
+	{
+		if ( ! value.StartsWith( NumberFormatInfo.CurrentInfo.NegativeSign ) )
+		{
+			if ( bSignPlus )	value = NumberFormatInfo.CurrentInfo.PositiveSign + value;
+			if ( bSignSpace )	value = ' ' + value;
+		}
+		return value;
+	}
+
+
+	static string ConvertInfinityOrNan( double v, char c )
+	{
+		if ( Char.IsUpper( c ) )
+		{
+			if ( Double.IsPositiveInfinity( v ) )
+				return "INFINITY";
+			else if ( Double.IsNegativeInfinity( v ) )
+				return NumberFormatInfo.CurrentInfo.NegativeSign + "INFINITY";
+			else if ( Double.IsNaN( v ) )
+				return "NAN";
+		}
+		else
+		{
+			if ( Double.IsPositiveInfinity( v ) )
+				return "infinity";
+			else if ( Double.IsNegativeInfinity( v ) )
+				return NumberFormatInfo.CurrentInfo.NegativeSign + "infinity";
+			else if ( Double.IsNaN( v ) )
+				return "nan";
+		}
+
+		return null;
+	}
+
+
+	static void AppendJustified( StringBuilder s, string value, int fieldWidth, bool bLeftJustify )
+	{
+		// Don't pad values that are larger than their fields.
+		if ( value.Length >= fieldWidth )
+		{
+			s.Append( value );
+			return;
+		}
+		
+		// Pad with spaces.
+		if ( bLeftJustify )
+		{
+			s.Append( value );
+			for ( int i = value.Length; i < fieldWidth; ++i )
+				s.Append( ' ' );
+		}
+		else
+		{
+			for ( int i = value.Length; i < fieldWidth; ++i )
+				s.Append( ' ' );
+			s.Append( value );
+		}
+	}
+
+
+	static void AppendPadded( StringBuilder s, string value, int fieldWidth )
+	{
+		// Don't pad values that are larger than their fields.
+		if ( value.Length >= fieldWidth )
+		{
+			s.Append( value );
+			return;
+		}
+
+		// Add sign/hexadecimal specifier.
+		int start = 0;
+		if ( value.StartsWith( NumberFormatInfo.CurrentInfo.PositiveSign ) )
+		{
+			s.Append( NumberFormatInfo.CurrentInfo.PositiveSign );
+			start = NumberFormatInfo.CurrentInfo.PositiveSign.Length;
+		}
+		else if ( value.StartsWith( NumberFormatInfo.CurrentInfo.NegativeSign ) )
+		{
+			s.Append( NumberFormatInfo.CurrentInfo.NegativeSign );
+			start = NumberFormatInfo.CurrentInfo.NegativeSign.Length;
+		}
+		else if ( value.StartsWith( "0x" ) || value.StartsWith( "0X" ) )
+		{
+			s.Append( value, 0, 2 );
+			start = 2;
+		}
+
+		// Add padding.
+		for ( int i = value.Length; i < fieldWidth; ++i )
+			s.Append( '0' );
+
+		// Add value.
+		s.Append( value, start, value.Length - start );
 	}
 
 
