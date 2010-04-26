171002208cb12657ca4fe1b0b9ad7765ddb2d10b
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2010-04-26 16:16:44 +0000

    String patterns not implemented yet.
    git-svn-id: http://svn.birotanker.com/Apollua@148 7c9f8f60-b31a-3540-8745-75118cae9e49

---
+++ 2010/Lua5.1/Library/string.pattern.cs
@@ -0,0 +1,25 @@
+﻿// string.pattern.cs
+//
+// Lua 5.1 is copyright © 1994-2008 Lua.org, PUC-Rio, released under the MIT license
+// This file © 2009 Edmund Kapusniak
+
+
+using System;
+
+
+namespace Lua.Library
+{
+
+
+public static partial class @string
+{
+
+
+
+
+
+
+}
+
+
+}
